dd37ca5a8e43adcec0555df59aed5600df0c923b
Martinehegg <gic016@uib.no>
2026-03-19 17:46:29 +0100

    debug game

--- TaskThree/script.js
+++ TaskThree/script.js
@@ -2,7 +2,7 @@
 let score = 0;
 
 // 2. Grab references to all moles
-const moles = document.getElementsByClassName("moles");
+const moles = document.getElementsByClassName("mole");
 
 // 3. Function to show one random mole at a time
 function showRandomMole() {
@@ -12,14 +12,14 @@ function showRandomMole() {
     }
 
     // Show a random one
-    const randomIndex = Math.floor(Math.random() * moles.lenght);
+    const randomIndex = Math.floor(Math.random() * moles.length);
     moles[randomIndex].style.display = "block";
 }
 
 // 4. Function to "whack" a mole
 function whackMole() {
     // Increase the score
-    scor += 1;
+    score += 1;
     document.getElementById("score").innerText = score;
 
     // Hide the mole you just hit
@@ -27,16 +27,36 @@ function whackMole() {
 }
 
 // 5. Function to start the game
+
+let gameInterval;
+
 function startGame() {
     // Reset score
     score = 0;
+    document.getElementById("startBtn").innerText = "Reset";
     document.getElementById("score").innerText = score;
-
+    
+    clearInterval(gameInterval)
     // Show a random mole every second
-    setInterval(showRandomMole, 1000);
+    
+    gameInterval=setInterval(showRandomMole, 1000);
 }
 
 // 6. Attach click event to each mole
-for (let i = 0; i <= moles.length; i++) {
+for (let i = 0; i < moles.length; i++) {
     moles[i].addEventListener("click", whackMole);
 }
+
+/* Edited:
+Changed from "score" to "startBtn" in function startGame
+
+Removed s frome moles in //2
+
+Fixed the miss spelled word length in moles.length
+
+Removed = from the for loop at the bottom. This for loop will search for mole[5] wich does not exist.
+
+Made the game possible to reset with changing the start button text from updating the score to say Reset.
+Also made a new variable gameInterval to be able to clear the game. 
+This way it is not possible to start multiple games at once.
+*/
